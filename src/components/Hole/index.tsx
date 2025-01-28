b8ea9747c8c4a76101d7f93c456555066cbb1816
Harsh Rohila <rohilaharsh@gmail.com>
2025-01-28 21:53:33 +0530

    added mole and dirt images

--- src/components/Hole/index.tsx
+++ src/components/Hole/index.tsx
@@ -1,7 +1,8 @@
 import { styled } from "@app/libs/style"
 import { GamePageService } from "@app/services/GamePageService"
 import React, { FC } from "react"
-
+import dirt from "@app/assets/mole-hill.png"
+import mole from "@app/assets/mole-head.png"
 interface HoleProps {
   showMole: boolean
 }
@@ -9,8 +10,31 @@ interface HoleProps {
 const StyledHole = styled.div<{ $showMole: boolean }>`
   width: 120px;
   height: 150px;
-  border: 1px solid black;
-  background-color: ${(props) => (props.$showMole ? "brown" : "green")};
+  /* border: 1px solid black; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: end;
+  overflow: hidden;
+  .mole {
+    width: 59%;
+    overflow: hidden;
+
+    position: relative;
+    top: 17px;
+    left: -4px;
+    img {
+      width: 100%;
+      // apppy transition based on transform
+      transition: transform 0.1s;
+      transform: ${(props) =>
+        props.$showMole ? "translateY(0)" : "translateY(50px)"};
+    }
+  }
+  .dirt {
+    position: relative;
+    width: 100%;
+  }
 `
 
 const Hole: FC<HoleProps> = (props) => {
@@ -22,7 +46,10 @@ const Hole: FC<HoleProps> = (props) => {
 
   return (
     <StyledHole onClick={handleClick} $showMole={props.showMole}>
-      Hole
+      <div className="mole">
+        <img src={mole} alt="mole" />
+      </div>
+      <img src={dirt} className="dirt" alt="dirt" />
     </StyledHole>
   )
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 const Config = {
   HOLES_COUNT: 8,
   MOLE_INTERVAL_IN_MS: 1000,
-  GAME_DURATION_IN_SEC: 15,
+  GAME_DURATION_IN_SEC: 1500,
 }
 
 export default Config
